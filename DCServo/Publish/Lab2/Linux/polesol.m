291d4c4eac854ab8f1f32076cd488e551715d178
Jordan87M <jmm72@case.edu>
2018-03-05 10:08:40 -0500

    finished first draft of solutions

--- DCServo/Publish/Lab2/Linux/polesol.m
+++ DCServo/Publish/Lab2/Linux/polesol.m
@@ -3,11 +3,19 @@ tau = .265;
 s = tf('s');
 K = [1,2,4,8,16,32,64];
 
-systf = ks/(s*(tau*s + 1));
+systf = ks/(s*(tau*s + 1))
+figure(1);
+%rlocus(systf,[0:.1:64])
+hold on
 
 for i = 1:length(K)
-    fbsystf = feedback(K(i)*systf,1);
-    %fbsystf = feedback(systf,K(i));
     display(['poles for ' num2str(K(i)) ' : '])
-    pole(fbsystf)
+    fbsystf = feedback(K(i)*systf,1)
+    %fbsystf = feedback(systf,K(i));
+    
+    poles = pole(fbsystf)
+    plot(real(poles),imag(poles),'*','MarkerSize',12)
+    wn = min(abs(poles))
+    zeta = min(abs(real(poles)))/wn
+    rlocfind(systf,poles)
 end
